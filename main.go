a60503577206bf72325542b79bc25b5d8a7ffcb4
alpern <alain.pernelle@gmail.com>
2020-06-27 16:53:54 +0000

    add book

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/emicklei/go-restful"
+	"github.com/alpern95/go-restful-api/book"
+	"github.com/alpern95/go-restful-api/db"
+)
+
+func main() {
+    db.InitDB()
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	wsContainer := restful.NewContainer()
+	wsContainer.Add(book.BookController{}.AddRouters())
+
+	// Add container filter to enable CORS
+	cors := restful.CrossOriginResourceSharing{
+		AllowedHeaders: []string{"Content-Type", "Accept", "Authorization"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		CookiesAllowed: false,
+		Container:      wsContainer}
+	wsContainer.Filter(cors.Filter)
+
+	host := "192.168.1.32:8080"
+	log.Printf("listening on: %s", host)
+	server := &http.Server{Addr: host, Handler: wsContainer}
+	log.Fatal(server.ListenAndServe())
+}
