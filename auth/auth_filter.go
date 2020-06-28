641ba8a0e83d18a0430e2661848bbf5a508fc775
alpern <alain.pernelle@gmail.com>
2020-06-28 13:31:08 +0000

    add filter

---
+++ auth/auth_filter.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"strings"
+
+	"github.com/emicklei/go-restful"
+	"github.com/globalsign/mgo/bson"
+	"github.com/alpern95/go-restful-api/db"
+)
+
+// BearerAuth is used by all other endpoints to performan bearer token authorization
+func BearerAuth(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
+	tokenHeader := req.HeaderParameter("Authorization")
+	if len(tokenHeader) == 0 {
+		resp.WriteErrorString(401, "not authorized")
+		return
+	}
+
+	splitted := strings.Split(tokenHeader, " ")
+	if len(splitted) != 2 {
+		resp.WriteErrorString(401, "not authorized")
+		return
+	}
+
+	// the real token
+	token := splitted[1]
+
+	session := db.NewDBSession()
+	defer session.Close()
+	c := session.DB("").C("user")
+	user := User{}
+	err := c.Find(bson.M{"token": token}).One(&user)
+
+	if err != nil {
+		resp.WriteErrorString(401, "Not Authorized")
+		return
+	}
+
+	chain.ProcessFilter(req, resp)
+}
