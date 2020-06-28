24698e101a128aff47ff5782e1fb0dd513d10486
alpern <alain.pernelle@gmail.com>
2020-06-28 10:04:43 +0000

    db

--- main.go
+++ main.go
@@ -5,28 +5,29 @@ import (
 	"net/http"
 
 	"github.com/emicklei/go-restful"
+	//"github.com/alpern95/go-restful-api/auth"
 	"github.com/alpern95/go-restful-api/book"
 	"github.com/alpern95/go-restful-api/db"
-	"github.com/alpern95/go-restful-api/auth"
 )
 
 func main() {
-    db.InitDB()
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	db.InitDB()
 
 	wsContainer := restful.NewContainer()
 	wsContainer.Add(book.BookController{}.AddRouters())
+	//wsContainer.Add(auth.UserController{}.AddRouters())
 
-    wsContainer.Add(auth.UserController{}.AddRouters())
 	// Add container filter to enable CORS
 	cors := restful.CrossOriginResourceSharing{
+	    AllowedDomains: []string{"http://192.168.1.32:3000"}, 
 		AllowedHeaders: []string{"Content-Type", "Accept", "Authorization"},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		CookiesAllowed: false,
 		Container:      wsContainer}
 	wsContainer.Filter(cors.Filter)
 
-	host := "192.168.1.32:8080"
+	host := "192.168.1.32:3000"
 	log.Printf("listening on: %s", host)
 	server := &http.Server{Addr: host, Handler: wsContainer}
 	log.Fatal(server.ListenAndServe())
