a2e22c9ea2cd26d44f889dcbb96b8c7bc0b204c3
alpern <alain.pernelle@gmail.com>
2020-07-06 11:31:03 +0000

    do.mod

--- borne/borne.go
+++ borne/borne.go
@@ -1,7 +1,7 @@
 package borne
 import (
 	"github.com/globalsign/mgo/bson" 
-	"github.com/alpern95/go-restful-api/credential"
+	//"github.com/alpern95/go-restful-api/credential"
 )
 
 type Borne struct {
